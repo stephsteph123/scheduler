e55ffcc635027764d6654b75c0d7297c75224103
stephsteph123 <stephaniepotterrowe@gmail.com>
2023-01-22 17:16:04 -0500

    update to application testing

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,6 +54,7 @@ const fixtures = {
 };
 
 export default {
+  defaults: { baseURL: "" },
   get: jest.fn(url => {
     if (url === "/api/days") {
       return Promise.resolve({
@@ -65,10 +66,20 @@ export default {
 
     if (url === "/api/appointments") {
       /* Resolve appointments data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
     }
 
     if (url === "/api/interviewers") {
       /* Resolve interviewers data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
     }
   })
 }
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,6 +6,11 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
