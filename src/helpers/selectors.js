0e367501fef29a536b42ac1d2e8f7fc2ebbb2916
stephsteph123 <stephaniepotterrowe@gmail.com>
2023-01-22 21:16:08 -0500

    update to hooks and helpers

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,3 +13,27 @@ export function getAppointmentsForDay(state, name) {
     }
     return filteredAppointments;
 }
+
+export  function getInterview(state, interview) {
+  if(!interview) return null;
+  const filteredInterview = {};
+  filteredInterview.student = interview.student;
+  filteredInterview.interviewer = state.interviewers[interview.interviewer];
+  return filteredInterview;
+}
+
+  export function getInterviewersForDay(state, name) {
+    const filteredDays = state.days.filter(day => day.name === name);
+    if(state.days.length===0 || filteredDays.length===0){
+      return [];
+    }
+    const interviewersFromDays = filteredDays[0].interviewers;
+   
+    let filteredInterviewers = [];
+  
+    for(let interviewer of interviewersFromDays) {
+      filteredInterviewers.push(state.interviewers[interviewer]);
+    }
+    return filteredInterviewers;
+
+}
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,116 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function useApplicationData() {
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => {
+    return setState({ ...state, day })
+  };
+
+  useEffect(()=>{
+    const dayURL = "/api/days";
+    const appointmentURL = "/api/appointments";
+    const interviewersURL = "/api/interviewers";
+    Promise.all([
+      axios.get(dayURL),
+      axios.get(appointmentURL),
+      axios.get(interviewersURL)
+    ]).then((all) =>{
+      setState(prev=>({...prev, days:all[0].data, appointments:all[1].data, interviewers:all[2].data}));
+    })
+  },[]);
+
+  function findDay(day) {
+    const daysOfWeek = {
+      Monday: 0,
+      Tuesday: 1,
+      Wednesday: 2,
+      Thursday: 3,
+      Friday: 4
+    }
+    return daysOfWeek[day]
+  }
+
+    function bookInterview(id, interview) {
+      const appointment = {
+        ...state.appointments[id],
+        interview: { ...interview }
+      };
+  
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment
+      };
+
+      const dayOfWeek = findDay(state.day)
+      let day = {
+        ...state.days[dayOfWeek],
+        spots: state.days[dayOfWeek]
+      }
+  
+      if (!state.appointments[id].interview) {
+        day = {
+          ...state.days[dayOfWeek],
+          spots: state.days[dayOfWeek].spots - 1
+        } 
+      } else {
+        day = {
+          ...state.days[dayOfWeek],
+          spots: state.days[dayOfWeek].spots
+        } 
+      }
+  
+      let days = state.days
+      days[dayOfWeek] = day;
+  
+  
+      const url =`/api/appointments/${id}`;
+      return axios.put(url, appointment).then(() => {
+        setState({...state, appointments,days});
+      })
+    }
+
+
+  function cancelInterview(id){
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    const dayOfWeek = findDay(state.day)
+
+    const day = {
+      ...state.days[dayOfWeek],
+      spots: state.days[dayOfWeek].spots + 1
+    }
+
+    let days = state.days
+    days[dayOfWeek] = day;
+
+    const url =`http://localhost:8001/api/appointments/${id}`;
+  
+    return axios.delete(url, appointment).then(()=>{
+      setState({...state, appointments, days });
+    });
+  }
+
+  return {
+    state, 
+    setDay,
+    bookInterview,
+    cancelInterview
+  }
+
+}
